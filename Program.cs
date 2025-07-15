5f4dc86300e6286b9af74ab7ea929d38dc023d5d
harris najar <harrisnajar522@gmail.com>
2025-07-15 16:43:42 +0530

    Add files via upload
    stack

--- Program.cs
+++ Program.cs
@@ -1,147 +1,100 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp59
+namespace ConsoleApp60
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-         
-        // array --- fixed size , data type secure
-        // list ---- flexible size ,data type secure
-        // dictionary --- flexible size, data type secure
-        // arraylist --- flexible size,  not data type secure
-        //queue 
-        //stack
-
-       // List<string> list = new List<string>() { "moin",123};
-        
-        ArrayList arrayList = new ArrayList() { "moin",123,0.5,"ahmad"};
-
-           for (int i = 0; i < arrayList.Count; i++) { 
-                Console.WriteLine(arrayList[i]);
-            }
-           
-           
 
 
-            foreach (var a in arrayList) { 
-                Console.WriteLine(a);
-            }
-        
-            arrayList.Add(1);
-            arrayList.Add("harris");
-            Console.WriteLine("---------------------");
-            foreach (var a in arrayList)
-            {
-                Console.WriteLine(a);
-            }
-            
-
-            arrayList.Remove("ahmad");
-            Console.WriteLine("---------------------");
-            foreach (var a in arrayList)
-            {
-                Console.WriteLine(a);
-            }
-            arrayList[0] = "tanfeez";
-            Console.WriteLine("---------------------");
-            foreach (var a in arrayList)
-            {
-                Console.WriteLine(a);
-            }
-            
-
-            arrayList.Insert(3, "burhan");
-            Console.WriteLine("---------------------");
-            foreach (var a in arrayList)
-            {
-                Console.WriteLine(a);
-            }
-            /*
-            arrayList.Add(1);
-            arrayList.Add(1);
-            arrayList.Add(1);
-          
-            Console.WriteLine(arrayList.Count);
-            arrayList.Clear();
-            Console.WriteLine(arrayList.Count);*/
+            Queue<string> queue1 = new Queue<string>();
+            queue1.Enqueue("moin");
+            queue1.Enqueue("ahmad");
+            queue1.Enqueue("basit");
+            queue1.Enqueue("umar");
+            queue1.Enqueue("sahil");
 
 
-              // ArrayList arraylist = new ArrayList() { "moin", 9682, 0.5, "ahmad" };
-
-          
-           // queue
-
-            List<string> list = new List<string>() { };
-
-        Queue q1 = new Queue();
-            q1.queefxn();
-          
-            
-           /* Console.WriteLine("------------------");
             foreach (var name in queue1)
             {
                 Console.WriteLine(name);
             }
+            
+            queue1.Enqueue("bilal");
+            Console.WriteLine("_______________");
+            foreach (var name in queue1)
+            {
+                Console.WriteLine(name);
+            }
+            queue1.Dequeue();
+
+            Console.WriteLine("_______________");
+
+            foreach (var name in queue1)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("_______________");
+            Console.WriteLine( queue1.Peek()+" :------peek element");
+            Console.WriteLine("_______________");
+            Console.WriteLine(queue1.Count);
+
+
+
+
+            /*Console.WriteLine("------------------");
+
+              int a;
+              string x;
+              int b = queue1.Count;
+
+              //Console.WriteLine(queue1.Count);
+
+              for (a = 0; a < b; a++)
+              {
+                  x = queue1.Dequeue();
+                  Console.WriteLine(x + " removed");
+                  //Console.WriteLine(queue1.Count);
+              }
+            */
+
+              Stack<string> stack1 = new Stack<string>();
+              stack1.Push("moin");
+              stack1.Push("ahmad");
+              stack1.Push("basit");
+              stack1.Push("Umar");
+              stack1.Push("sahil");
+              Console.WriteLine("------------------");
+              foreach (var name in stack1) {
+                  Console.WriteLine(name);
+              }
             Console.WriteLine("------------------");
-            Console.WriteLine(queue1.Peek());
-           */
+            stack1.Pop();
+            foreach (var name in stack1)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine( stack1.Peek());
 
 
+              string q;
+              int c = stack1.Count;
 
-
-
+              Console.WriteLine(queue1.Count);
+              Console.WriteLine("------------");
+              for (int m = 0; m < c; m++)
+              {
+                  q = stack1.Pop();
+                  Console.WriteLine(q+ " removed");
+                  Console.WriteLine(queue1.Count);
+              }
+            
         }
-
     }
-}
-
-
-class Queue {
-
-    public void queefxn() {
-
-
-        Queue<string> queue1 = new Queue<string>();
-        queue1.Enqueue("moin");
-        queue1.Enqueue("ahmad");
-        queue1.Enqueue("basit");
-        queue1.Enqueue("umar");
-        queue1.Enqueue("sahil");
-
-
-        foreach (var name in queue1)
-        {
-            Console.WriteLine(name);
-        }
-
-
-
-        Console.WriteLine("------------------");
-
-        int a;
-        string x;
-        int b = queue1.Count;
-        Console.WriteLine(queue1.Count);
-
-        for (a = 0; a < queue1.Count; a++)
-        {
-            x = queue1.Dequeue();
-            Console.WriteLine(x + " removed");
-        }
-
-
-
-
-
-    }
-
-
-
 }
